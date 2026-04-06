27b902b9bbbd4e4127bacd3a400a0cd7be64cf24
Batch1 <Batch1@VITUNIVERSITY.LOCAL>
2026-04-06 12:13:42 +0530

    first commit

---
+++ app.java
@@ -0,0 +1,12 @@
+package com.example;
+public class App {
+    public static void main(String[] args) {
+        int number = 29;
+        System.out.println(number + (isPrime(number) ? " is prime" : " is not prime"));
+    }
+    public static boolean isPrime(int n) {
+        if (n <= 1) return false;
+        for (int i = 2; i <= Math.sqrt(n); i++) if (n % i == 0) return false;
+        return true;
+    }
+}
