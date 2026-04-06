d48bbb2c822c655e98c7d33d91ca3f1b687ce7f2
alvinkbabu <alvin.kbabu2023@vitstudent.ac.in>
2026-04-06 12:24:16 +0530

    Update app.java

--- app.java
+++ app.java
@@ -1,4 +1,3 @@
-package com.example;
 public class App {
     public static void main(String[] args) {
         int number = 29;
@@ -10,3 +9,4 @@ public class App {
         return true;
     }
 }
+
